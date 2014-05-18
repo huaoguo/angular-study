94d22909876976dc097917464653181b34d76328
huaoguo <huaoguo@gmail.com>
2014-05-18 20:36:38 +0800

    改为用coffee、jade和grunt来学习angularjs

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['dist/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      build: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        options: {
+          bare: false
+        },
+        expand: true,
+        cwd: '',
+        src: ['coffee/**/*.coffee'],
+        dest: 'dist',
+        ext: '.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      build: {
+        src: 'dist/**/*.js'
+      }
+    },
+    clean: {
+      build: {
+        src: ['dist']
+      }
+    },
+    coffeelint: {
+      app: ['coffee/**/*.coffee'],
+    },
+    jade: {
+      build: {
+        expand: true,
+        src: ['view/**/*.jade'],
+        dest: 'dist',
+        ext: '.html'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      coffee: {
+        files: '*.coffee',
+        tasks: ['coffee', 'uglify']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'coffeelint', 'coffee', 'concat', 'uglify', 'jade']);
+
+};
